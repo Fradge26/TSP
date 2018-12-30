2b44478aae4904717f52c6b12e7caffa7510ca02
Adam Fradgley <fradge@hotmail.co.uk>
2018-12-30 22:52:46 +0000

    3-opt

--- TSP.py
+++ TSP.py
@@ -111,6 +111,20 @@ def cache_dist(n1, n2, n3, n4, adj_dict, ids2x, ids2y):
         adj_dict[(n2, n4)] = ((ids2x[n2] - ids2x[n4]) ** 2 + (ids2y[n2] - ids2y[n4]) ** 2) ** 0.5
 
 
+def cache_edge_distances(adj_dict, ids2x, ids2y, path, edges):
+    for e in edges:
+        if not (e[0], e[1]) in adj_dict:
+            adj_dict[(path[e[0]], path[e[1]])] = ((ids2x[path[e[0]]] - ids2x[path[e[1]]]) ** 2 +
+                                                  (ids2y[path[e[0]]] - ids2y[path[e[1]]]) ** 2) ** 0.5
+
+
+def cost_k_edges(adj_dict, route, edges):
+    cost = 0
+    for e in edges:
+        cost += adj_dict[(route[e[0]], route[e[1]])]
+    return cost
+
+
 def initial_path_from_nearest(ids, coords):
     kdtree = spatial.KDTree(coords)
     path = [0]
@@ -135,6 +149,7 @@ def find_crosses(path, score_func):
     '''
     obselete
     '''
+
     def ccw(a, b, c):
         return (ids2y[c] - ids2y[a]) * (ids2x[b] - ids2x[a]) > (ids2y[b] - ids2y[a]) * (ids2x[c] - ids2x[a])
         # return (c.y - a.y) * (b.x - a.x) > (b.y - a.y) * (c.x - a.x)
@@ -232,6 +247,11 @@ def run_2opt_chunks(score_func, ids2x, ids2y, time_limit, path):
     return best_path
 
 
+def swap_2opt_fast(path, i, k):
+    path[i:k] = path[k - 1:i - 1:-1]
+    return path
+
+
 def swap_2opt(route, i, k):
     """
     swaps the endpoints of two edges by reversing a section of nodes,
@@ -286,7 +306,7 @@ def run_2opt_n_dict(path, score_function, n_dict, ids2x, ids2y):
                     best_k = k
                     improvement = True
             if improvement:
-                best_path = swap_2opt(best_path, min(best_i, best_k), max(best_i, best_k))
+                best_path = swap_2opt_fast(best_path, min(best_i, best_k), max(best_i, best_k))
                 id2index = dict(zip(best_path, range(len(best_path))))
                 local_best_score = 0
                 roll_i = best_i
@@ -295,10 +315,69 @@ def run_2opt_n_dict(path, score_function, n_dict, ids2x, ids2y):
     return best_path, (datetime.datetime.now() - start)
 
 
+def run_3opt_n_dict(path, score_function, n_dict, ids2x, ids2y):
+    start = datetime.datetime.now()
+    improvement = True
+    best_path = path
+    local_best_score = 0
+    id2index = dict(zip(best_path, range(len(best_path))))
+    i_indices = range(1, len(best_path) - 1)
+    roll_i = 0
+    adj_dict = {}
+    while improvement:
+        improvement = False
+        for i in list(np.roll(i_indices, -roll_i)):
+            for j_node, _ in n_dict[best_path[i]].items():
+                j = id2index[j_node]
+                for k_node, _ in n_dict[best_path[j]].items():
+                    k = id2index[k_node]
+                    i, j, k = sorted([i, j, k])
+                    if j - i == 1 or k - j == 1 or len(set((i, j, k))) != 3: continue
+                    improvement = False
+                    n1, n2, n3, n4, n5, n6 = i - 1, i, j - 1, j, k - 1, k
+                    best_swap = [(n1, n2), (n3, n4), (n5, n6)]
+                    cache_edge_distances(adj_dict, ids2x, ids2y, best_path, best_swap)
+                    current_cost = cost_k_edges(adj_dict, best_path, best_swap)
+                    for sol in [[(n1, n2), (n3, n5), (n4, n6)],
+                                [(n1, n3), (n2, n4), (n5, n6)],
+                                [(n1, n3), (n2, n5), (n4, n6)],
+                                [(n1, n4), (n5, n2), (n3, n6)],
+                                [(n1, n4), (n5, n3), (n2, n6)],
+                                [(n1, n5), (n4, n2), (n3, n6)],
+                                [(n1, n5), (n4, n3), (n2, n6)]]:
+                        cache_edge_distances(adj_dict, ids2x, ids2y, best_path, sol)
+                        sol_cost = cost_k_edges(adj_dict, best_path, sol)
+                        if sol_cost < current_cost:
+                            best_swap = sol
+                            current_cost = sol_cost
+                            improvement = True
+                    if improvement:
+                        if best_swap[1][0] > best_swap[0][1]:
+                            s2 = 1
+                        else:
+                            s2 = -1
+                        if best_swap[2][0] > best_swap[1][1]:
+                            s3 = 1
+                        else:
+                            s3 = -1
+                        best_path = best_path[:best_swap[0][0] + 1] + \
+                                     best_path[best_swap[0][1]:best_swap[1][0] + s2:s2] + \
+                                     best_path[best_swap[1][1]:best_swap[2][0] + s3:s3] + \
+                                     best_path[best_swap[2][1]:]
+                        roll_i = i
+                        break
+                else:
+                    continue
+                break
+
+    assert len(best_path) == len(path)
+    return best_path, (datetime.datetime.now() - start)
+
+
 if __name__ == '__main__':
-    plots = True
+    plots = False
     annotate = False
-    sample_size = int(197769/197769*4999)  # 197769 total
+    sample_size = int(197769 / 197769 * 499)  # 197769 total
 
     cities = np.genfromtxt('Inputs/cities.csv', delimiter=',', skip_header=True)
     sub_cities = cities[:sample_size, :]
@@ -319,11 +398,11 @@ if __name__ == '__main__':
     print(f"Creating dictionary of close neighbours")
     neighbours_dict = create_neighbours_dict(ids, coords, 100)
     print(f"Initial score {init_score}")
-    print("Running 2-opt")
-    best_path, rt = run_2opt_n_dict(init_path, update_score_from_dict, neighbours_dict, ids2x, ids2y)
+    print("Running 3-opt")
+    best_path, rt = run_3opt_n_dict(init_path, update_score_from_dict, neighbours_dict, ids2x, ids2y)
     best_score = get_score_prime(best_path, ids2x, ids2y)
     write_submission(best_path, best_score)
-    print(f"2-opt complete! Score:{best_score}, Time:{rt}")
+    print(f"3-opt complete! Score:{best_score}, Time:{rt}")
     if plots:
         plot_path(sample_size, best_path, best_score, annotate)
 
---
+++ test.py
@@ -0,0 +1,112 @@
+import numpy as np
+import pickle
+import datetime
+
+
+def get_score(route, cost_mat):
+    score = 0
+    for i in range(len(route) - 1):
+        score += cost_mat[route[i]][route[i + 1]]
+    return score
+
+
+def cost_k_edges(cost_mat, route, edges):
+    cost = 0
+    for e in edges:
+        cost += cost_mat[route[e[0]]][route[e[1]]]
+    return cost
+
+
+def cost_change(cost_mat, n1, n2, n3, n4):
+    return cost_mat[n1][n3] + cost_mat[n2][n4] - cost_mat[n1][n2] - cost_mat[n3][n4]
+
+
+def three_opt(route, cost_mat):
+    start = datetime.datetime.now()
+    best_route = route
+    improved = True
+    while improved:
+        for i in range(1, len(route) - 6):
+            for j in range(i + 2, len(route) - 4):
+                for k in range(j + 2, len(route) - 2):
+                    improved = False
+                    n1, n2, n3, n4, n5, n6 = i - 1, i, j - 1, j, k - 1, k
+                    best_swap = [(n1, n2), (n3, n4), (n5, n6)]
+                    current_cost = cost_k_edges(cost_mat, best_route, best_swap)
+                    for sol in [[(n1, n2), (n3, n5), (n4, n6)],
+                                [(n1, n3), (n2, n4), (n5, n6)],
+                                [(n1, n3), (n2, n5), (n4, n6)],
+                                [(n1, n4), (n5, n2), (n3, n6)],
+                                [(n1, n4), (n5, n3), (n2, n6)],
+                                [(n1, n5), (n4, n2), (n3, n6)],
+                                [(n1, n5), (n4, n3), (n2, n6)]]:
+                        sol_cost = cost_k_edges(cost_mat, best_route, sol)
+                        if sol_cost < current_cost:
+                            best_swap = sol
+                            current_cost = sol_cost
+                            improved = True
+                    if improved:
+                        if best_swap[1][0] > best_swap[0][1]:
+                            s2 = 1
+                        else:
+                            s2 = -1
+                        if best_swap[2][0] > best_swap[1][1]:
+                            s3 = 1
+                        else:
+                            s3 = -1
+                        best_route = best_route[:best_swap[0][0] + 1] + \
+                                     best_route[best_swap[0][1]:best_swap[1][0] + s2:s2] + \
+                                     best_route[best_swap[1][1]:best_swap[2][0] + s3:s3] + \
+                                     best_route[best_swap[2][1]:]
+                        break
+            route = best_route
+    return best_route, datetime.datetime.now() - start
+
+
+def two_opt(route, cost_mat):
+    start = datetime.datetime.now()
+    best = route
+    improved = True
+    local_best_score = 0
+    i_indices = range(1, len(route) - 2)
+    roll_i = 0
+    while improved:
+        improved = False
+        for i in list(np.roll(i_indices, -roll_i)):
+            for j in range(i + 1, len(route)):
+                if j - i == 1: continue
+                if cost_change(cost_mat, best[i - 1], best[i], best[j - 1], best[j]) < local_best_score:
+                    best_i, best_j = i, j
+                    # best[best_i:best_j] = best[best_j - 1:best_i - 1:-1]
+                    improved = True
+            if improved:
+                best[best_i:best_j] = best[best_j - 1:best_i - 1:-1]
+                local_best_score = 0
+                roll_i = best_i
+                break
+        route = best
+    return best, datetime.datetime.now() - start
+
+
+if __name__ == '__main__':
+    nodes = 75
+    init_route = list(range(nodes))
+
+    cost_mat = np.random.randint(100, size=(nodes, nodes))
+    cost_mat += cost_mat.T
+    np.fill_diagonal(cost_mat, 0)
+    cost_mat = list(cost_mat)
+    pickle.dump(cost_mat, open("./Pickle/cost_mat.p", "wb"))
+
+    cost_mat = pickle.load(open("./Pickle/cost_mat.p", "rb"))
+    print(f"inital route score {get_score(init_route, cost_mat)}")
+
+    print("Running 2-opt")
+    best_route, run_time = two_opt(init_route, cost_mat)
+    print(f"2-opt route score {get_score(best_route, cost_mat)}, time:{run_time}")
+
+    print("Running 3-opt")
+    best_route, run_time = three_opt(init_route, cost_mat)
+    print(f"3-opt route score {get_score(best_route, cost_mat)}, time:{run_time}")
+
+
